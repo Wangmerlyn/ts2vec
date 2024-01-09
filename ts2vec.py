e58ffa43480ecbb6ab98794d211169ce9f0b63a3
Wang Siyuan <wsy0227@sjtu.edu.cn>
2024-01-09 15:50:54 +0800

    changed ts2vec import

--- ts2vec.py
+++ ts2vec.py
@@ -2,9 +2,9 @@ import torch
 import torch.nn.functional as F
 from torch.utils.data import TensorDataset, DataLoader
 import numpy as np
-from models import TSEncoder
-from models.losses import hierarchical_contrastive_loss
-from misc import (
+from .models import TSEncoder
+from .models.losses import hierarchical_contrastive_loss
+from .misc import (
     take_per_row,
     split_with_nan,
     centerize_vary_length_series,
